3a56f29d968a5a4d5de4a3deb586945dc5226f8b
Niklas Lindenbaum <niklas.lindenbaum@t-online.de>
2022-11-12 18:28:56 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
+  const image =
+    "social-issue/src/builder-construction-vest-orange-helmet-standing-white-studio.jpg";
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1>Super cool awesome project</h1>
+      <img src={image} alt="Construction worker" />
+      <p>Please be patient! The project is still under construction!</p>
     </div>
   );
 }
