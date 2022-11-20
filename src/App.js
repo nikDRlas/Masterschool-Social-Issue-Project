83ed480ae3259dfb883bbb7f8b4e5715c6dc959b
ronnenn podolsky <podolsky.ronnen@gmail.com>
2022-11-20 20:39:52 +0200

    dummy change in App h1

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
     "social-issue/src/builder-construction-vest-orange-helmet-standing-white-studio.jpg";
   return (
     <div className="container">
-      <h1>Super cool awesome project</h1>
+      <h1>Super cool awesome project - Niklas Ronnen Snir</h1>
       <img src={image} alt="Construction worker" />
       <p>Please be patient! The project is still under construction!</p>
     </div>
