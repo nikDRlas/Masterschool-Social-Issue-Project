36fa52432b4cf111c9a3a26c05ce0d7d852c15ec
ronnenn podolsky <podolsky.ronnen@gmail.com>
2022-11-21 20:40:40 +0200

    tailwind basic

--- src/App.js
+++ src/App.js
@@ -5,8 +5,7 @@ function App() {
     "social-issue/src/builder-construction-vest-orange-helmet-standing-white-studio.jpg";
   return (
     <div className="container">
-      <nav></nav>
-      <h1>Super cool awesome project - Niklas Snir</h1>
+      <h1 className="text-3xl font-bold underline">Super cool awesome project - Niklas Ronnen Snir</h1>
       <img src={image} alt="Construction worker" />
       <p>Please be patient! The project is still under construction!</p>
     </div>
