cd9a12fb8e710258678161719f5becbc1abf320b
Niklas Lindenbaum <niklas.lindenbaum@t-online.de>
2022-11-22 09:39:29 +0100

    added main part

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
+import Main from "./components/main/main";
 
 function App() {
-  const image =
-    "social-issue/src/builder-construction-vest-orange-helmet-standing-white-studio.jpg";
   return (
     <div className="container">
-      <h1 className="text-3xl font-bold underline">Super cool awesome project - Niklas Ronnen Snir</h1>
-      <img src={image} alt="Construction worker" />
-      <p>Please be patient! The project is still under construction!</p>
+      <Main />
     </div>
   );
 }
