e924fd4d82e9e91d47d4b5693a1213cc8a978b14
Snir <snirazran1@gmail.com>
2022-11-23 18:03:03 +0200

    edited signup page

--- src/pages/signup/Signup.js
+++ src/pages/signup/Signup.js
@@ -2,32 +2,56 @@ import { Link } from "react-router-dom";
 import Navbar from "../../components/navbar/Navbar";
 import { useAuth } from "../../context/AuthContext";
 
-
 function Signup() {
   const { register } = useAuth();
 
-
   return (
     <>
-
       <Navbar />
-      <Link to='/'>
+      <Link to="/">
         <h1>homepage</h1>
       </Link>
-      <h1 className="text-4xl b">
-        Hello<span>!</span>
-      </h1>
-      <p>Create a free account with your email.</p>
-      <form>
-        <input type="text" />
-        <input type="email" />
-        <input type="number" />
-        <input type="password" />
+      <div className="top-text text-center">
+        <h1 className="font-medium text-7xl">
+          Hello<span className="text-darkGreen">!</span>
+        </h1>
+        <p className="text-lightGray mt-10 ">
+          Create a free account with your email.
+        </p>
+      </div>
+      <form className="flex flex-col w-96 items-center  mx-auto my-4">
+        <input
+          className="w-96 m-4 border border-veryLightGray p-3 rounded-lg"
+          type="text"
+          placeholder="Full Name"
+        />
+        <input
+          className="w-96 m-4 border border-veryLightGray p-3 rounded-lg"
+          type="email"
+          placeholder="Email"
+        />
+        <input
+          className="w-96 m-4 border border-veryLightGray p-3 rounded-lg"
+          type="tel"
+          placeholder="Phone Number"
+        />
+        <input
+          className="w-96 m-4 border border-veryLightGray p-3 rounded-lg"
+          type="password"
+          placeholder="Password"
+        />
+        <button className="w-96 m-4 bg-darkGreen text-white py-4 px-3 rounded-lg">
+          Create your free account
+        </button>
       </form>
-      <button>Create your free account</button>
-      <p>
-        Already has an account? <Link to='/login'>login</Link>
-      </p>
+      <div className="bottom-text text-center">
+        <p className="text-lightGray">
+          Already has an account?{" "}
+          <Link className="text-darkGreen" to="/login">
+            login
+          </Link>
+        </p>
+      </div>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,30 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     screens: {
-      sm: '480px',
-      md: '768px',
-      lg: '976px',
-      xl: '1440px'
+      sm: "480px",
+      md: "768px",
+      lg: "976px",
+      xl: "1440px",
     },
     extend: {
       colors: {
-        oliveGreen: 'hsl(118, 36%, 42%)',
-        brightRed: 'hsl(12, 88%, 59%)',
-        brightRedLight: 'hsl(12, 88%, 69%)',
-        brightRedSupLight: 'hsl(12, 88%, 95%)',
-        darkBlue: 'hsl(228, 39%, 23%)',
-        darkGrayishBlue: 'hsl(227, 12%, 61%)',
-        veryDarkBlue: 'hsl(233, 12%, 13%)',
-        veryPaleRed: 'hsl(13, 100%, 96%)',
-        veryLightGray: 'hsl(0, 0%, 98%)',
+        oliveGreen: "hsl(118, 36%, 42%)",
+        brightRed: "hsl(12, 88%, 59%)",
+        brightRedLight: "hsl(12, 88%, 69%)",
+        brightRedSupLight: "hsl(12, 88%, 95%)",
+        darkBlue: "hsl(228, 39%, 23%)",
+        darkGrayishBlue: "hsl(227, 12%, 61%)",
+        veryDarkBlue: "hsl(233, 12%, 13%)",
+        veryPaleRed: "hsl(13, 100%, 96%)",
+        darkGreen: "#6A994E",
+        lightGreen: "#A7C957",
+        veryLightGreen: "#F2E8CF",
+        lightGray: "#919191",
+        veryLightGray: "#BFBFBF",
       },
     },
   },
   plugins: [],
-}
+};
