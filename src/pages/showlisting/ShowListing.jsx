a0cc7f31322761e709db05efc21df45a749ead67
ronnenn podolsky <podolsky.ronnen@gmail.com>
2022-12-01 16:27:42 +0200

    added date posted to showlisting

--- src/pages/showlisting/ShowListing.jsx
+++ src/pages/showlisting/ShowListing.jsx
@@ -5,12 +5,12 @@ import Navbar from "../../components/navbar/Navbar";
 import { useAuth } from "../../context/AuthContext";
 
 import house from "../../images/house.jpg";
-import shower from "../../images/icons/shower.svg";
+import ac from "../../images/icons/ac.svg";
 import babies from "../../images/icons/babies.svg";
 import pets from "../../images/icons/pets.svg";
+import shower from "../../images/icons/shower.svg";
 import tv from "../../images/icons/tv.svg";
 import wifi from "../../images/icons/wifi.svg";
-import ac from "../../images/icons/ac.svg";
 import { db, doc, getDoc } from "../../utils/firebase";
 
 const ShowListing = () => {
@@ -43,28 +43,29 @@ const ShowListing = () => {
 
   if (loading)
     return (
-      <div className="text-center mt-60 text-6xl">
+      <div className="text-6xl text-center mt-60">
         Loading<span className="text-darkGreen">...</span>
       </div>
     );
-  if (error) return <div className="text-center mt-60 text-4xl">{error}</div>;
+  if (error) return <div className="text-4xl text-center mt-60">{error}</div>;
 
   return (
     <div>
       {user ? <LogedNavbar /> : <Navbar />}
       {console.log(listing)}
-      <h1 className="text-5xl md:text-6xl font-medium leading-10 text-center">
+      <h1 className="text-5xl font-medium leading-10 text-center md:text-6xl">
         Stay with {listing.fullName}
         <span className="text-darkGreen">!</span>
       </h1>
-      <div className="lex-row flex justify-center mx-auto my-14 w-auto">
+      <div className="flex justify-center w-auto mx-auto lex-row my-14">
         <div className="flex flex-col items-center w-1/2 md:w-1/3">
           <img
-            className="w-3/4 md:2/3 lg:w-7/12 h-auto"
+            className="w-3/4 h-auto md:2/3 lg:w-7/12"
             src={house}
             alt="a house"
           />
-          <h2 className="mt-3 font-semibold">{listing.fullName}</h2>
+          <h2 className="mt-3 text-sm italic text-lightGray">{listing?.timeStamp?.toDate().toLocaleString()}</h2>
+          <h2 className="font-semibold ">{listing.fullName}</h2>
           <h2 className="font-semibold">{listing.city}</h2>
           <p className="text-sm text-lightGray">{listing.email}</p>
           <p className="text-sm text-lightGray">{listing.phone}</p>
@@ -74,7 +75,7 @@ const ShowListing = () => {
           <p className="my-3">
             What {listing?.fullName?.split(" ")[0]} offers:
           </p>
-          <div className="mb-3 flex w-1/2 gap-2">
+          <div className="flex w-1/2 gap-2 mb-3">
             {listing.wifi && <img className="w-1/6 h-auto" src={wifi} alt="wifi" />}
             {listing.ac && <img className="w-1/6 h-auto" src={ac} alt="ac" />}
             {listing.pets && <img className="w-1/6 h-auto" src={pets} alt="pets" />}
@@ -85,25 +86,25 @@ const ShowListing = () => {
           <h2 className="mt-3">Available dates:</h2>
 
           <div className="flex">
-            <div className="my-4 p-2 text-xs text-lightGray border-2 rounded-l-lg	lg:w-32 sm:w-18 md:w-24">
+            <div className="p-2 my-4 text-xs border-2 rounded-l-lg text-lightGray lg:w-32 sm:w-18 md:w-24">
               <p>FROM</p>
               <p className="">{listing.from}</p>
             </div>
 
-            <div className="my-4 p-2 text-xs text-lightGray border-2 rounded-r-lg lg:w-32 sm:w-18 md:w-24">
+            <div className="p-2 my-4 text-xs border-2 rounded-r-lg text-lightGray lg:w-32 sm:w-18 md:w-24">
               <p>UNTIL</p>
               <p className="">{listing.until}</p>
             </div>
           </div>
 
           <h2>Number of guests:</h2>
-          <div className="my-4 p-2 text-xs text-lightGray border-2 rounded-lg lg:w-64 sm:w-36 md:w-48">
+          <div className="p-2 my-4 text-xs border-2 rounded-lg text-lightGray lg:w-64 sm:w-36 md:w-48">
             <p>UP TO</p>
             <p className="">{listing.guestNum} guests</p>
           </div>
           <button
             
-            className=" bg-darkGreen text-white py-4 px-3 rounded-lg lg:w-64 sm:w-36 md:w-48 "
+            className="px-3 py-4 text-white rounded-lg bg-darkGreen lg:w-64 sm:w-36 md:w-48"
           >
             Contact now
           </button>
