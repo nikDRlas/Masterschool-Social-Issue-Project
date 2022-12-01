b52648ad55b40d1c10c017743ab94e02ac5508ec
ronnenn podolsky <podolsky.ronnen@gmail.com>
2022-12-01 19:25:30 +0200

    added scroll to about and how it works with a prop func

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import { FiMenu } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
 import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({scrollToSection}) => {
   const [showMenu, setShowMenu] = useState(false);
 
   const toggle = () => {
@@ -11,29 +11,29 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="realtive mx-auto p-6">
-      <div className="flex content-center items-center justify-between ">
+    <nav className="p-6 mx-auto realtive">
+      <div className="flex items-center content-center justify-between ">
         <div className="pt-2">
           <Link to={"/"}>
             <p className="text-3xl font-bold">
               Wegot
-              <span className=" text-oliveGreen font-bold text-3xl">U</span>
+              <span className="text-3xl font-bold text-oliveGreen">U</span>
             </p>
           </Link>
         </div>
-        <div className="hidden pt-2 md:flex items-center justify-between gap-3 text-sm">
-          <p className="hover:text-darkGrayishBlue mx-2">How does it work?</p>
-          <p className="hover:text-darkGrayishBlue mx-2">About us</p>
+        <div className="items-center justify-between hidden gap-3 pt-2 text-sm md:flex">
+          <p onClick={() => scrollToSection('how-it-works')} className="mx-2 hover:text-darkGrayishBlue">How does it work?</p>
+          <p onClick={() => scrollToSection('about')} className="mx-2 hover:text-darkGrayishBlue">About us</p>
           <Link to="/signin">
             <p
-              className="hover:text-darkGrayishBlue mx-2 pl-6 py-2"
+              className="py-2 pl-6 mx-2 hover:text-darkGrayishBlue"
               style={{ borderLeftWidth: "1px" }}
             >
               Sign in
             </p>
           </Link>
           <Link to="/signup">
-            <button className="mx-2 hover:text-darkBlue text-white p-1 px-2 rounded-md bg-oliveGreen">
+            <button className="p-1 px-2 mx-2 text-white rounded-md hover:text-darkBlue bg-oliveGreen">
               Sign up
             </button>
           </Link>
@@ -47,10 +47,10 @@ const Navbar = () => {
       {showMenu && (
         <div
           id="menu"
-          className="md:hidden items-center divide-y-4 flex-col flex mt-3 space-y-3 bg-white sm:w-auto sm:self-center drop-shadow-md text-xs"
+          className="flex flex-col items-center mt-3 space-y-3 text-xs bg-white divide-y-4 md:hidden sm:w-auto sm:self-center drop-shadow-md"
         >
-          <p className="hover:text-darkGrayishBlue">How does it work?</p>
-          <p className="hover:text-darkGrayishBlue">About us</p>
+          <p onClick={() => scrollToSection('how-it-works')} className="hover:text-darkGrayishBlue">How does it work?</p>
+          <p onClick={() => scrollToSection('about')} className="hover:text-darkGrayishBlue">About us</p>
           <Link to="/signin">
             <p className="hover:text-darkGrayishBlue">Sign In</p>
           </Link>
---
+++ src/pages/main/main.jsx
@@ -0,0 +1,109 @@
+import { useNavigate } from "react-router-dom";
+import LogedNavbar from "../../components/navbar/LogedNavbar";
+import { useAuth } from "../../context/AuthContext";
+import Navbar from "../../components/navbar/Navbar";
+import howitwork from "../../images/howitwork.png";
+import snirPhoto from "../../images/snir.png";
+import niklasPhoto from "../../images/niklas.png";
+import ronenPhoto from "../../images/ronen.png";
+
+const Main = () => {
+  const { user } = useAuth();
+  const navigate = useNavigate();
+  
+  const scrollToSection = (section) =>{
+    document.querySelector(`.${section}`).scrollIntoView({ behavior: 'smooth'})
+  }
+
+  
+  return (
+    <>
+      {user ? <LogedNavbar scrollToSection={scrollToSection} /> : <Navbar scrollToSection={scrollToSection} />}
+      <div className="h-screen mb-20 main-container ">
+        {/* Hero Section */}
+
+        <div className="bg-center bg-no-repeat bg-cover image-container bg-hero-pattern h-max">
+          <div className="w-2/3 m-auto text-center text-container pt-28 lg:w-3/5 sm:w-4/5 xsm:w-4/5">
+            <h1 className="font-bold text-white main-text lg:text-6xl sm:text-4xl xsm:text-3xl ">
+              find or give a place of comfort in tough times.
+            </h1>
+            <h2 className="mt-32 text-white sec-text lg:text-5xl sm:text-3xl xsm:text-2xl ">
+              I would like to
+            </h2>
+          </div>
+          <div className="flex flex-col items-center justify-center gap-12 mt-12 button-container pb-28">
+            <button
+              onClick={() => {
+                if (user) {
+                  navigate("/addlisting");
+                } else {
+                  navigate("/signin");
+                }
+              }}
+              className="h-20 text-4xl text-center rounded-md w-80 bg-hostBtn lg:w-96 sm:w-72 xsm:w-56"
+            >
+              Host
+            </button>
+            <button className="h-20 text-4xl text-center rounded-md w-80 bg-hostedBtn lg:w-96 sm:w-72 xsm:w-56">
+              Be Hosted
+            </button>
+          </div>
+        </div>
+
+        {/* How it works Section */}
+        <div className="flex flex-col items-center px-1 mt-32 how-it-works">
+          <h1 className="font-bold lg:text-5xl sm:text-3xl xsm:text-2xl">
+            How Does It Work?
+          </h1>
+          <img
+            className="w-2/4 mt-24 xlg: lg:w-2/4 sm:w-4/5 xsm:w-full"
+            src={howitwork}
+            alt=""
+          />
+          <h2 className="w-3/5 mt-6 font-medium text-center xlg: lg:w-3/5 sm:w-4/5 xsm:w-full text-lightGray lg:text-4xl sm:text-3xl xsm:text-2xl">
+            We help people at risk find shelter by connecting them with the
+            perfects hosts
+          </h2>
+        </div>
+
+        {/* About us Section */}
+        <div className="flex flex-col items-center px-1 mt-32 about">
+          <h1 className="font-bold lg:text-5xl sm:text-3xl xsm:text-2xl">
+            About Us
+          </h1>
+          <div className="flex items-center justify-center mt-24 circle-images gap-x-20 gap-y-20 lg:flex-row sm:flex-col xsm:flex-col ">
+            <div className="circle-image lg:w-1/5 sm:w-3/4 xsm:w-3/4">
+              <img src={snirPhoto} alt="" />
+              <h3 className="mt-6 text-2xl font-medium text-center">
+                Snir Azran
+              </h3>
+            </div>
+            <div className="circle-image lg:w-1/5 sm:w-3/4 xsm:w-3/4">
+              <img src={niklasPhoto} alt="" />
+              <h3 className="mt-6 text-2xl font-medium text-center">
+                Niklas Lindenbaum
+              </h3>
+            </div>
+            <div className="circle-image lg:w-1/5 sm:w-3/4 xsm:w-3/4">
+              <img src={ronenPhoto} alt="" />
+              <h3 className="mt-6 text-2xl font-medium text-center">
+                Ronnen Podolsky
+              </h3>
+            </div>
+          </div>
+          <div>
+            
+          </div>
+          <h2 className="mt-32 mb-32 font-medium text-center xlg:w-3/5 lg:w-3/5 text-lightGray lg:text-4xl sm:text-3xl xsm:text-2xl sm:w-4/5 xsm:w-full">
+            This is a group project developed for MasterSchool
+          </h2>
+
+        </div>
+
+        <div className="circle circle-left-top"></div>
+      </div>
+    </>
+  );
+};
+
+export default Main;
