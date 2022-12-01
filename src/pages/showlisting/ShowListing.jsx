634ed9d01018bf015e992827f28dbd462eb039b3
Snir <snirazran1@gmail.com>
2022-12-01 07:12:07 +0200

    fixed variable naming for your page

--- src/pages/showlisting/ShowListing.jsx
+++ src/pages/showlisting/ShowListing.jsx
@@ -53,7 +53,7 @@ const ShowListing = () => {
     <div>
       {user ? <LogedNavbar /> : <Navbar />}
       <h1 className="text-5xl md:text-6xl font-medium leading-10 text-center">
-        Stay with {listing.name}
+        Stay with {listing.fullName}
         <span className="text-darkGreen">!</span>
       </h1>
       <div className="lex-row flex justify-center mx-auto my-14 w-auto">
@@ -63,14 +63,16 @@ const ShowListing = () => {
             src={house}
             alt="a house"
           />
-          <h2 className="mt-3 font-semibold">{listing.name}</h2>
+          <h2 className="mt-3 font-semibold">{listing.fullName}</h2>
           <h2 className="font-semibold">{listing.city}</h2>
           <p className="text-sm text-lightGray">{listing.email}</p>
           <p className="text-sm text-lightGray">{listing.phone}</p>
         </div>
         <div className="flex flex-col w-1/2 md:w-1/3">
-          <p className=" text-lightGray">"{listing.aboutYou}"</p>
-          <p className="my-3">What {listing?.name?.split(" ")[0]} offers:</p>
+          <p className=" text-lightGray">"{listing.aboutU}"</p>
+          <p className="my-3">
+            What {listing?.fullName?.split(" ")[0]} offers:
+          </p>
           <div className="mb-3 flex w-1/2 gap-2">
             <img className="w-1/6 h-auto" src={wifi} alt="a house" />
             <img className="w-1/6 h-auto" src={ac} alt="a house" />
@@ -81,7 +83,7 @@ const ShowListing = () => {
           </div>
           <p>change to true/false and render the images conditionally</p>
           <p>
-            babies: {listing.babies} wifi: {listing.wifi}
+            babies: {listing.baby} wifi: {listing.wifi}
           </p>
           <p>
             ac: {listing.ac} shower: {listing.shower}
@@ -94,19 +96,19 @@ const ShowListing = () => {
           <div className="flex">
             <div className="my-4 p-2 text-xs text-lightGray border-2 rounded-l-lg	lg:w-32 sm:w-18 md:w-24">
               <p>FROM</p>
-              <p className="">{listing.arriveDate}</p>
+              <p className="">{listing.from}</p>
             </div>
 
             <div className="my-4 p-2 text-xs text-lightGray border-2 rounded-r-lg lg:w-32 sm:w-18 md:w-24">
               <p>UNTIL</p>
-              <p className="">{listing.leavingDate}</p>
+              <p className="">{listing.until}</p>
             </div>
           </div>
 
           <h2>Number of guests:</h2>
           <div className="my-4 p-2 text-xs text-lightGray border-2 rounded-lg lg:w-64 sm:w-36 md:w-48">
             <p>UP TO</p>
-            <p className="">{listing.numOfGuests} guests</p>
+            <p className="">{listing.guestNum} guests</p>
           </div>
           <button
             button
